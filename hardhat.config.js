034aca3b874a5ff2d1b295b5ea2a2f42fa3bc035
Salazar254 <asherkiiru777@gmail.com>
2026-06-22 13:44:55 +0300

    Switch to Hardhat 3, ethers 6, remove flashbots bundle

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,16 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+<<<<<<< HEAD
 import '@nomicfoundation/hardhat-ethers';
+=======
+import '@nomicfoundation/hardhat-ethers';   // switched to Hardhat 3 plugin
+>>>>>>> 99b3761 (Switch to Hardhat 3, ethers 6, remove flashbots bundle)
 
-export default {
+import { defineConfig } from 'hardhat/config';
+
+export default defineConfig({
   solidity: {
     compilers: [{ version: '0.8.19' }]
   },
   networks: {
     base: {
       url: process.env.BASE_RPC_PUBLIC || 'https://mainnet.base.org',
-      chainId: 8453
+      chainId: 8453,
+      type: "http"
     }
   }
-};
+});
